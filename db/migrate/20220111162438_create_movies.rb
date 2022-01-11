efdc004983f5ec029781c219e9f94a74237beded
Alex Vigreux <alex.vigreux@kedgebs.com>
2022-01-11 17:32:17 +0100

    DB's schema implemented

--- db/migrate/20220111162438_create_movies.rb
+++ db/migrate/20220111162438_create_movies.rb
@@ -2,7 +2,7 @@ class CreateMovies < ActiveRecord::Migration[6.1]
   def change
     create_table :movies do |t|
       t.string :title
-      t.content :overview
+      t.string :overview
       t.string :poster_url
       t.integer :rating
 
